3643c59ad5ad41b90242144780290c223e075ab2
HannaBerggren <hannajberggren@gmail.com>
2023-06-20 17:34:11 +0000

    Add Post properties in Post.js file

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,7 +1,26 @@
 import React from "react";
 import styles from "../../styles/Post.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const Post = (props) => {
+  const {
+      id,
+      owner,
+      profile_id,
+      profile_image,
+      comments_count,
+      likes_count,
+      like_id,
+      title,
+      content,
+      image,
+      updated_at,
+    } = props;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+  
+
   return <div>Post placeholder text</div>;
 };
 
