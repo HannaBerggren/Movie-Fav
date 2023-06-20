1fdc209ac45d2a510f2a654aa982131ea16660f6
HannaBerggren <hannajberggren@gmail.com>
2023-06-20 21:16:19 +0000

    Create useEffect, fechPosts function

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -13,6 +13,22 @@ function PostsPage({ message, filter = "" }) {
     const [posts, setPosts] = useState({ results: [] });
     const [hasLoaded, setHasLoaded] = useState(false);
     const { pathname } = useLocation();
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+          try {
+            const { data } = await axiosReq.get(`/posts/?${filter}`);
+            setPosts(data);
+            setHasLoaded(true);
+          } catch (err) {
+            console.log(err);
+          }
+        };
+    
+        setHasLoaded(false);
+        fetchPosts();
+      }, [filter, pathname]);
+      
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
