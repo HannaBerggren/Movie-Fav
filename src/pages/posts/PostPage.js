52be363ae682375772e13c33f8eac5c57e10e27b
HannaBerggren <hannajberggren@gmail.com>
2023-06-21 16:28:09 +0000

    Add axiosReq for comments

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -23,11 +23,12 @@ function PostPage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }] = await Promise.all([
+        const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?post=${id}`),
         ]);
         setPost({ results: [post] });
-        console.log(post);
+        setComments(comments);
       } catch (err) {
         console.log(err);
       }
