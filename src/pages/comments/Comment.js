bf5fb5f3d3b6d96f3d514c46fdc398b9a92706a4
HannaBerggren <hannajberggren@gmail.com>
2023-06-21 18:47:59 +0000

    Create useCurrentUser and is_owner variable, add required import

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -3,9 +3,14 @@ import { Media } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import styles from "../../styles/Comment.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
 
 const Comment = (props) => {
   const { profile_id, profile_image, owner, updated_at, content } = props;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
 
   return (
     <div>
