b595cbad94c7e5469bcb9ad4b71a9c4ca9740b77
HannaBerggren <hannajberggren@gmail.com>
2023-06-22 18:07:01 +0000

    Create custom hooks for useProfileData and useSetProfileData

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -1,4 +1,7 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 const ProfileDataContext = createContext();
 const setProfileDataContext = createContext();
+
+export const useProfileData = () => useContext(ProfileDataContext);
+export const useSetProfileData = () => useContext(SetProfileDataContext);
