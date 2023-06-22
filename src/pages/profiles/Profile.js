fa4245a661f58e81125f4ecd4da838fc7f15d60c
HannaBerggren <hannajberggren@gmail.com>
2023-06-22 16:30:38 +0000

    Add Profile prop and add variables to destructure data, add required import

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,8 +1,16 @@
 import React from "react";
 import styles from "../../styles/Profile.module.css";
 import btnStyles from "../../styles/Button.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
-const Profile = () => {
+const Profile = (props) => {
+  const { profile, mobile, imageSize = 55 } = props;
+  const { id, following_id, image, owner } = profile;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+  
   return <div></div>; 
 };
 
