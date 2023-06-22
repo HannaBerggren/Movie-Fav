57f52327899486c89b89c0f4c09feef87e5e94fb
HannaBerggren <hannajberggren@gmail.com>
2023-06-22 15:58:07 +0000

    Add Profile.module.css file and add .WordBreak

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -33,13 +33,24 @@ const PopularProfiles =({ mobile }) => {
   }, [currentUser]);
 
   return (
-    <Container className={appStyles.Content}>
+    <Container className={`${appStyles.Content} ${
+        mobile && "d-lg-none text-center mb-3"
+      }`}
+    >
       {popularProfiles.results.length ? (
         <>
-          <p>Most Followed Profiles!</p>
-          {popularProfiles.results.map((profile) => (
-            <p key={profile.id}>{profile.owner}</p>
-          ))}
+          <p>Most followed profiles.</p>
+          {mobile ? (
+            <div className="d-flex justify-content-around">
+              {popularProfiles.results.slice(0, 4).map((profile) => (
+                <p key={profile.id}>{profile.owner}</p>
+              ))}
+            </div>
+          ) : (  
+            popularProfiles.results.map((profile) => (
+                <p key={profile.id}>{profile.owner}</p>
+              ))
+            )}
         </>
       ) : (
         <Asset spinner />
