4f6a99279847e50cf7f90a69b120179dea1e000c
HannaBerggren <hannajberggren@gmail.com>
2023-06-22 12:59:23 +0000

    Add profileData and setProfileData variables using useState hook, import useState

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import appStyles from "../../App.module.css";
 
 const PopularProfiles =() => {
+  const [profileData, setProfileData] = useState({
+    // we will use the pageProfile later!
+    pageProfile: { results: [] },
+    popularProfiles: { results: [] },
+  });
+
   return (
     <Container className={appStyles.Content}>
       <p>Most Followed Profiles!</p>  
