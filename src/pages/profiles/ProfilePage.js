e50b1021f408fe22ceea93ffde0c43e6a13991dc
HannaBerggren <hannajberggren@gmail.com>
2023-06-23 08:48:50 +0000

    Add useState hook with profilePost/setProfilePost, add API request to owner profile

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -22,6 +22,7 @@ import { Button, Image } from "react-bootstrap";
 
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
+  const [profilePosts, setProfilePosts] = useState({ results: [] });
   const currentUser = useCurrentUser();
   const { id } = useParams();
   const setProfileData = useSetProfileData();
@@ -32,13 +33,15 @@ function ProfilePage() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [{data: pageProfile}] = await Promise.all([
+        const [{ data: pageProfile}, { data: profilePosts }] = await Promise.all([
           axiosReq.get(`/profiles/${id}/`),
-        ])
+          axiosReq.get(`/posts/?owner__profile=${id}`),
+        ]);
         setProfileData((prevState) => ({
           ...prevState,
-          pageProfile: {results: [pageProfile]}
-        }))
+          pageProfile: {results: [pageProfile]},
+        }));
+        setProfilePosts(profilePosts);
         setHasLoaded(true);
       } catch(err){
         console.log(err);
