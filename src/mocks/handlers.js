4b10ff48964b920ef1b874c64d436f70664ddbb5
HannaBerggren <hannajberggren@gmail.com>
2023-06-25 13:24:12 +0000

    Add rest.get and rest.post, add return statements

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -3,7 +3,21 @@ import { rest } from "msw";
 const baseURL = "https://movie-fav-project-5.herokuapp.com/"
 
 export const handlers = [
-    rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
-      return res(ctx.json{})  
-    })
+  rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        pk: 3,
+        username: "Elsa",
+        email: "",
+        first_name: "",
+        last_name: "",
+        profile_id: 3,
+        profile_image: 
+         "https://res.cloudinary.com/hannacloud/image/upload/v1/media/../movie-profile",
+      })
+    );  
+  }),
+  rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
 ];
